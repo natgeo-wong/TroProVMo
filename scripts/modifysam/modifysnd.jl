a35dc553790ea49d911398d4f9cd744d8e9d08cb
Nathanael Wong <natgeo.wong@outlook.com>
2024-03-22 23:05:18 -0400

    Some standardization

--- scripts/modifysam/modifysnd.jl
+++ scripts/modifysam/modifysnd.jl
@@ -15,13 +15,13 @@ snddata[:,2] .= p
 snddata[:,4] .= q
 
 snddata[:,3] .= (t.+0.05) .* (1000 ./p).^(287/1004)
-createsndmean("$(expii)_warm",snddata;psfc=1009.32)
+createsndmean("$(expii)_warm.snd",snddata;psfc=1009.32)
 
 snddata[:,3] .= (t.-0.05) .* (1000 ./p).^(287/1004)
-createsndmean("$(expii)_cool",snddata;psfc=1009.32)
+createsndmean("$(expii)_cool.snd",snddata;psfc=1009.32)
 
 snddata[:,3] .= (t.+0.10) .* (1000 ./p).^(287/1004)
-createsndmean("$(expii)_hot",snddata;psfc=1009.32)
+createsndmean("$(expii)_hot.snd",snddata;psfc=1009.32)
 
 snddata[:,3] .= (t.-0.10) .* (1000 ./p).^(287/1004)
-createsndmean("$(expii)_cold",snddata;psfc=1009.32)
+createsndmean("$(expii)_cold.snd",snddata;psfc=1009.32)
