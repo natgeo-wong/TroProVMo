930817b03f4f6611343a24b38c3c41e6e6e0591a
Nathanael Wong <natgeo.wong@outlook.com>
2024-03-27 19:48:42 -0400

    Adding WTGLargeScale experiments

---
+++ scripts/WTGLargeScale/modifyprm.jl
@@ -0,0 +1,50 @@
+using DrWatson
+@quickactivate "WTGLargeScale"
+using Logging
+using Printf
+
+include(srcdir("common.jl"))
+include(srcdir("sam.jl"))
+
+prjname = "WTGLargeScale"
+schname = "DGW"
+radname = "P"
+
+if schname == "DGW"
+    wtgvec = [0.02,0.05,0.1,0.2,0.5,1,2,5,10,20,50]
+else
+    wtgvec = [sqrt(2),2,2*sqrt(2.5),5,5*sqrt(2)]
+    wtgvec = vcat(wtgvec/10,1,wtgvec)
+end
+wlsvec = vcat(-1:0.2:2); wlsvec = wlsvec[.!iszero.(wlsvec)]
+
+oprm = rundir("prmtemplates",schname,"$(radname).prm";prjname)
+
+for wls in wlsvec
+
+    expname = wlsname(wls)
+    folname = prmdir(schname,radname,expname;prjname); mkpath(folname)
+
+    for wtgii in wtgvec
+
+        if schname == "DGW"
+              wtgdmp = wtgii; wtgrlx = 1
+        else; wtgrlx = wtgii; wtgdmp = 1
+        end
+
+        pwrname = powername(wtgii,schname)
+        open(joinpath(folname,"$(pwrname).prm"),"w") do fprm
+            open(oprm,"r") do rprm
+                s = read(rprm,String)
+                s = replace(s,"[runname]" => pwrname)
+                s = replace(s,"[en]"      => "$(imember)")
+                s = replace(s,"[am]"      => @sprintf("%7e",wtgdmp))
+                s = replace(s,"[tau]"     => @sprintf("%7e",wtgrlx))
+                write(fprm,s)
+            end
+        end
+        @info "Creating new prm file for $prjname $schname $radname $expname $pwrname"
+
+    end
+
+end
---
+++ scripts/WTGLargeScale/modifyrun.jl
@@ -0,0 +1,54 @@
+using DrWatson
+@quickactivate "WTGLargeScale"
+using Printf
+
+include(srcdir("common.jl"))
+include(srcdir("sam.jl"))
+
+prjname = "WTGLargeScale"
+schname = "DGW"
+radname = "P"
+email   = ""
+doBuild = true
+
+if schname == "DGW"
+    wtgvec = [0.02,0.05,0.1,0.2,0.5,1,2,5,10,20,50]
+else
+    wtgvec = [sqrt(2),2,2*sqrt(2.5),5,5*sqrt(2)]
+    wtgvec = vcat(wtgvec/10,1,wtgvec)
+end
+wlsvec = vcat(-1:0.2:2); wlsvec = wlsvec[.!iszero.(wlsvec)]
+
+mfid = open(runtemplatedir("modelrun.sh";prjname),"r"); str_m = read(mfid,String)
+bfid = open(runtemplatedir("Build.csh";  prjname),"r"); str_b = read(bfid,String)
+
+for wls in wlsvec
+    runname = wlsname(wls)
+    folname = rundir(schname,radname,runname;prjname)
+    for wtgii in wtgvec
+
+        open(joinpath(folname,"$(pwrname).sh"),"w") do wrun
+            nstr_m = replace(str_m ,"[email]"   => email)
+            nstr_m = replace(nstr_m,"[exproot]" => expdir(prjname))
+            nstr_m = replace(nstr_m,"[schname]" => schname)
+            nstr_m = replace(nstr_m,"[radname]" => radname)
+            nstr_m = replace(nstr_m,"[runname]" => runname)
+            nstr_m = replace(nstr_m,"[pwrname]" => powername(wtgii,schname))
+            write(wrun,sn)
+        end
+
+        if doBuild
+            open(joinpath(folname,"Build.csh"),"w") do wrun
+                nstr_b = replace(str_b ,"[datadir]" => datadir(prjname))
+                nstr_b = replace(nstr_b,"[schname]" => schname)
+                nstr_b = replace(nstr_b,"[radname]" => radname)
+                nstr_b = replace(nstr_b,"[runname]" => runname)
+                write(wrun,sn)
+            end
+        end
+
+    end
+end
+
+close(mfid)
+close(bfid)
