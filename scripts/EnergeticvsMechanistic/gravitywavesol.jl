2b43e54a27e66cc9eb4b74db6990e1f2e73e25cd
Nathanael Wong <natgeo.wong@outlook.com>
2024-04-03 18:08:20 -0400

    Trying to optimize and make it fast

--- scripts/EnergeticvsMechanistic/gravitywavesol.jl
+++ scripts/EnergeticvsMechanistic/gravitywavesol.jl
@@ -4,27 +4,25 @@ using DrWatson
 using Dates
 using DifferentialEquations
 using FFTW
-using ForwardDiff
-using Interpolations
 using JLD2
 using Logging
 using LinearAlgebra
 using SparseArrays
 
-@info "$(now()) - TroProVMo - Internal gravity wave calculation"
+@info "$(now()) - TroProVMo - Internal gravity wave calculation"; flush(stderr)
 
 # Constants
 N2 = 0.01^2  # square of buoyancy frequency, in 1/second^2
 L = 40e6     # horizontal domain size, in m
-H = 30e4      # model top height, in m
-T = 100 * 86400  # integration time, in seconds
+H = 20e3      # model top height, in m
+T = 10 * 86400  # integration time, in seconds
 
 # Grid settings
-nx = 200; nt = 120; nz = 600
-dx = L / nx; dz = H / (nz + 1); dt = T / nt
+nx = 200; const nz = 400
+dx = L / nx; dz = H / (nz + 1)
 x = ((1:nx) .- 0.5) .* dx
 z = (1:nz) .* dz
-t = (1:nt) .* dt
+@info "$(now()) - TroProVMo - Domain is $(nx) in x by $(nz) in z"; flush(stderr)
 
 # Heating definition
 xscale = 1.e6    # in m
@@ -36,54 +34,58 @@ Qk = fft(Qx)
 # Qz = 1.0 .* sin.(pi .* min.(1, z ./ zscale))
 Qz = z .< zscale
 
-Q = Qz * Qk'
+const Q = Qz * Qk'
+Qri = reinterpret(Float64,Q)
+const Qr = Qri[1:2:end,:]
+const Qi = Qri[2:2:end,:]
 
 # Laplacian matrix
 e1 = ones(nz)
 A = spdiagm(0 => -2*e1, 1 => ones(nz-1), -1 => ones(nz-1)) / dz^2
 b = zeros(nz)
+Ainv = inv(Matrix(A))
 
 # Horizontal wavenumber
 alfa = collect(0:nx-1) ./ L
 alfa[alfa .> nx/2 ./ L] .-= nx / L
-alfa *= 2 * π
+const alfac = alfa * 2 * π
+const nalfa = nx ÷ 2 + 1
+const npnts = nz * nalfa
 
-nalfa = nx ÷ 2 + 1
-
-@info "$(now()) - TroProVMo - Initializing Jacobian for computation ..."
+@info "$(now()) - TroProVMo - Initializing Jacobian for computation ..."; flush(stderr)
 # Jacobian computation
-Jacobian = spzeros(nz*nalfa, nz*nalfa)
+const Jacobian = zeros(nz*nalfa, nz*nalfa)
 for i = 2:nalfa
     ioffset = nz * (i - 1)
-    Jacobian[ioffset.+(1:nz), ioffset.+(1:nz)] = (alfa[i]^2) * (N2) .* inv(Matrix(A))
+    Jacobian[ioffset.+(1:nz), ioffset.+(1:nz)] = (alfac[i]^2) * (N2) .* Ainv
 end
-fullJacobian = spzeros(2*nz*nalfa, 2*nz*nalfa)
-fullJacobian[nz*nalfa.+(1:nz*nalfa),1:(nz*nalfa)] .= Jacobian
-fullJacobian[1:(nz*nalfa),nz*nalfa.+(1:nz*nalfa)] .= I(nz*nalfa)
+const JacobianT = sparse(Jacobian')
 
+@info "$(now()) - TroProVMo - Setting up Initial Conditions and Forcings ..."; flush(stderr)
 # Initial condition
 y0 = zeros(2*nz*nalfa,2)
+const forcing1 = zeros(npnts)
+const forcing2 = zeros(npnts)
+const tmpmat   = zeros(npnts)
+for i = 2:nalfa
+    ioffset = nz * (i - 1)
+    forcing1[ioffset+1:ioffset+nz] = -(alfac[i]^2) .* Qr[:,i]
+    forcing2[ioffset+1:ioffset+nz] = -(alfac[i]^2) .* Qi[:,i]
+end
 
-@info "$(now()) - TroProVMo - Initializing ODE System for computation ..."
+@info "$(now()) - TroProVMo - Defining ODE System of Equations ..."; flush(stderr)
 function ode_fun!(dydt, y, p, t)
-    (nalfa, nz, Jacobian, alfa, Qt) = p
-    forcing1 = zeros(nz*nalfa)
-    forcing2 = zeros(nz*nalfa)
-    for i = 2:nalfa
-        ioffset = nz * (i - 1)
-        forcing1[ioffset+1:ioffset+nz] = -(alfa[i]^2) .* Qt[1:2:end,i]
-        forcing2[ioffset+1:ioffset+nz] = -(alfa[i]^2) .* Qt[2:2:end,i]
-    end
-    dydt[1:nz*nalfa,1] = y[nz*nalfa+1:end,1]
-    dydt[(nz*nalfa+1):end,1] = forcing1 + Jacobian * y[1:nz*nalfa,1]
-    dydt[1:nz*nalfa,2] = y[nz*nalfa+1:end,2]
-    dydt[(nz*nalfa+1):end,2] = forcing2 + Jacobian * y[1:nz*nalfa,2]
+    (nza, J, forcinga, forcingb, tmpmat) = p
+    dydt[1:nza,1] .= y[nza.+(1:nza),1]; dydt[nza.+(1:nza),1] .= forcinga
+    dydt[1:nza,2] .= y[nza.+(1:nza),2]; dydt[nza.+(1:nza),2] .= forcingb
+    mul!(tmpmat,J,view(y,1:nza,1)); dydt[nza.+(1:nza),1] .+= tmpmat
+    mul!(tmpmat,J,view(y,1:nza,2)); dydt[nza.+(1:nza),2] .+= tmpmat
     return 
 end
 
 # Setup and solve the ODE
-@info "$(now()) - TroProVMo - Solving the ODE computation ..."
-prob = ODEProblem(ode_fun!, y0, (0.0, T), (nalfa, nz, Jacobian, alfa, reinterpret(Float64,Q)))
+@info "$(now()) - TroProVMo - Solving the ODE computation ..."; flush(stderr)
+prob = ODEProblem(ode_fun!, y0, (0.0, T), (npnts, JacobianT, forcing1, forcing2, tmpmat))
 sol = solve(prob, alg_hints = [:stiff], reltol=1e-6, abstol=1e-10, saveat=3600)
 
 w = zeros(nz,nx,length(sol.t))
@@ -92,12 +94,12 @@ buoy = zeros(nz,nx,length(sol.t))
 fw = zeros(ComplexF64,nz,200)
 fb = zeros(ComplexF64,nz,200)
 
-@info "$(now()) - TroProVMo - Converting output from Fourier Space to Real Space ..."
+@info "$(now()) - TroProVMo - Converting output from Fourier Space to Real Space ..."; flush(stderr)
 for i in 1 : length(sol.t)
 
     fw[:,1:101] = reshape(sol.u[i][1:nz*nalfa,1], nz, nalfa) .+ im * reshape(sol.u[i][1:nz*nalfa,2], nz, nalfa)
     fb[:,1:101] = -reshape(sol.u[i][nz*nalfa+1:2*nz*nalfa,1], nz, nalfa) .- im * reshape(sol.u[i][nz*nalfa+1:2*nz*nalfa,2], nz, nalfa)
-    fb[:,2:nalfa] = fb[:, 2:nalfa] ./ (alfa[2:nalfa] .^ 2)'  # Broadcasting operation
+    fb[:,2:nalfa] = fb[:, 2:nalfa] ./ (alfac[2:nalfa] .^ 2)'  # Broadcasting operation
 
     for j in (nalfa+1):nx
         fw[:, j] = conj(fw[:, nx-j+2])
