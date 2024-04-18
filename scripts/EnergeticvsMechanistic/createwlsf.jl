fbcafaea4246a532226c0e78d3a2f3486106f284
Nathanael Wong <natgeo.wong@outlook.com>
2024-04-18 17:13:25 -0400

    Create vertical velocity profiles based on gravitywave solutions

--- scripts/EnergeticvsMechanistic/createwlsf.jl
+++ scripts/EnergeticvsMechanistic/createwlsf.jl
@@ -1,6 +1,8 @@
 using DrWatson
 @quickactivate "TroProVMo"
 
+using Interpolations
+
 include(srcdir("samsnd.jl"))
 include(srcdir("samlsf.jl"))
 
@@ -9,7 +11,14 @@ radname = "P_FSF"
 
 z,p,_,_,_,_ = readsnd("$(radname).snd";prjname); nz = length(z)
 
-function wforcing(z,p=zeros(length(z));zbl,w₀)
+function wforcing(z,p=zeros(length(z));w₀)
+
+    ds  = NCDataset(datadir("gravitywave.nc"))
+	zi	= ds["z"][:]
+	wi 	= ds["w"][:,:,:]
+	close(ds)
+
+    itp = interpolate(zi,wi)
 
     nz = length(z)
     lsfdata = zeros(nz,7)
@@ -19,20 +28,16 @@ function wforcing(z,p=zeros(length(z));zbl,w₀)
         lsfdata[:,2] .= p
     end
 
-    w = w₀ .* (z - (z.^2)/2/zbl)/zbl*2
-    if w₀ > 0
-        w[w.<0] .= 0
-    else
-        w[w.>0] .= 0
+    for iz = 1 : nz
+        lsfdata[iz,7] = itp(zi[iz]) * w₀
     end
-    lsfdata[:,7] .= w
     
     return lsfdata
 
 end
 
-for w in vcat(0,0.1:0.1:0.5,1:5)/10
+for w in vcat(0:0.1:1)/10
     wstring = @sprintf("%04.2f",w)
     fid = joinpath(radname,"w_$(wstring)mps.lsf")
-    printlsf(fid,wforcing(z,p,zbl=1500,w₀=w),1009.32;prjname)
+    printlsf(fid,wforcing(z,p,w₀=w),1009.32;prjname)
 end
